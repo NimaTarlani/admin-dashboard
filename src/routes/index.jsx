83af1b4f19277a97b12e5aa6d1d046b8c8dd9b85
Nima Tarlani <nimatarlani@hotmail.com>
2025-10-21 19:09:56 +0330

    add route and layout

---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../layouts/Layout";
+import Home from "../pages/Home";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
